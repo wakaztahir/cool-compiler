1d31099e8f9e52a4d3a61867d739e62f17528e4b
Waqas Tahir <wakaztahir@gmail.com>
2021-09-23 21:10:47 +0500

    cool compier

---
+++ cmake-build-debug/CMakeFiles/CoolCompiler.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CoolCompiler.dir/main.cpp.obj"
+  "CoolCompiler.exe"
+  "CoolCompiler.exe.manifest"
+  "CoolCompiler.lib"
+  "CoolCompiler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CoolCompiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
